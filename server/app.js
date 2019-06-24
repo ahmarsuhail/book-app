86cc73b431cb7f7c003dbfac7261489da5535438
Ahmar Suhail <ahmar.suhail@gmail.com>
2019-06-24 16:57:58 -0700

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+mongoose.connect('mongodb://ahmar:ahmar123@ds047335.mlab.com:47335/library');
+mongoose.connection.once('open', () => {
+    console.log("Connected");
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));    
+
+app.listen(4000, () => {
+    console.log("Now listenting");
+});
